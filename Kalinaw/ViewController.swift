b61256dde21509eeca0b71039d3fb08fdafb2b78
Lui Tolentino <louise19kriztine@yahoo.com>
2022-06-17 21:29:44 +0800

    hi

--- Kalinaw/ViewController.swift
+++ Kalinaw/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
         let article1URL = URL(string:"https://blog.doctorondemand.com/why-its-important-to-care-for-your-mental-health-834c8670b889")
           UIApplication.shared.open(article1URL!)
     }
+  
+    @IBAction func SourcesButton(_ sender: Any) {
+        let sourcesURL = URL(string:"https://docs.google.com/document/d/1gq2mKGhNusf_JxKZJ3xz0eb2GehFfGiu-2lcZw-yKS8/edit?usp=sharing")
+        UIApplication.shared.open(sourcesURL!)
+    }
+    
     
 }
 
