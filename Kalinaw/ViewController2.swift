608201f0eb83451e47f3023db732af7cf2579a05
Jasmine Gannaoui <jasminegannaoui@gmail.com>
2022-06-17 02:49:22 -0400

    Links updated!!

--- Kalinaw/ViewController2.swift
+++ Kalinaw/ViewController2.swift
@@ -15,11 +15,29 @@ class ViewController2: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func firstLInk(_ sender: Any) {
+    @IBAction func firstLink(_ sender: Any) {
         let article1URL = URL(string:"https://blog.doctorondemand.com/why-its-important-to-care-for-your-mental-health-834c8670b889")
           UIApplication.shared.open(article1URL!)
     }
     
+    
+    @IBAction func secondLink(_ sender: Any) {
+        let article2URL = URL(string:"https://www.takingcharge.csh.umn.edu/how-does-nature-impact-our-wellbeing#:~:text=Being%20in%20nature%2C%20or%20even,the%20production%20of%20stress%20hormones")
+          UIApplication.shared.open(article2URL!)
+    }
+    @IBAction func thirdLink(_ sender: Any) {let article3URL = URL(string:"https://www.mentalhealth.org.uk/campaigns/nature/nature-research#:~:text=People%20with%20good%20nature%20connectedness%20tend%20to%20be%20happier&text=Nature%20can%20generate%20a%20multitude,lower%20depression%20and%20anxiety%20levels")
+        UIApplication.shared.open(article3URL!)
+    }
+    
+    @IBAction func fourthLink(_ sender: Any) {
+        let video2URL = URL(string:"https://youtu.be/tEmt1Znux58")
+         UIApplication.shared.open(video2URL!)
+       }
+    
+    @IBAction func fifthLinkWOO(_ sender: Any) {
+        let video3URL = URL(string:"https://youtu.be/NQcYZplTXnQ")
+          UIApplication.shared.open(video3URL!)
+    }
     /*
     // MARK: - Navigation
 
--- Kalinaw/ViewController3M.swift
+++ Kalinaw/ViewController3M.swift
@@ -14,8 +14,31 @@ class ViewController3M: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func firstLinkE(_ sender: Any) {let article4URL = URL(string:"https://oceanservice.noaa.gov/ocean/earthday.html")
+        UIApplication.shared.open(article4URL!)
+    }
     
-
+    @IBAction func secondLinkE(_ sender: Any) {
+        let article5URL = URL(string:"https://www.inspirecleanenergy.com/blog/sustainable-living/ways-to-protect-the-environment")
+          UIApplication.shared.open(article5URL!)
+    }
+    
+    @IBAction func thirdLinkE(_ sender: Any) {
+        let article6URL = URL(string:"https://www.gvi.co.uk/blog/how-to-cultivate-environmental-awareness-in-schools/")
+          UIApplication.shared.open(article6URL!)
+    }
+    
+    @IBAction func fourthLinkE(_ sender: Any) {
+        let video1URL = URL(string:"https://youtu.be/jdDsXdtcuLQ")
+          UIApplication.shared.open(video1URL!)
+    }
+    
+    @IBAction func fifthLinkEWOO(_ sender: Any) {
+        let video4URL = URL(string:"https://youtu.be/h29z-l3XTlk")
+        UIApplication.shared.open(video4URL!)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
